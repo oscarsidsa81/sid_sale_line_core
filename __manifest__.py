6090b0bc602e020a830fd987547fed5b05a282d3
oscarsidsa81 <ovilches@sid-sa.com>
2026-02-26 16:57:55 +0100

    Commit inicial

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "SID - Sale Order Line Core",
+    "name": "sid_sale_line_core",
     "version": "15.0.1.0.0",
     "category": "Sales",
     "summary": "Campos core y estados auxiliares en sale.order.line.",
-    "author": "SIDSA / Custom",
+    "author": "oscarsidsa81",
     "license": "LGPL-3",
     "depends": ["sale", "sale_stock", "stock", "oct_fecha_contrato_ventas"],
     "data": [],
